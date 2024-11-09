b6aa35e7a59a11f7d4cd9ebd860704e97435552f
ddori314 <won030104@gmail.com>
2024-11-09 22:07:13 +0900

    홈화면, 검색창

--- app/src/main/java/com/example/sigi/HomFragment.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sigi
-
-import androidx.fragment.app.Fragment
-
-class HomeFragment : Fragment(R.layout.fragment_home)
---
+++ app/src/main/java/com/example/sigi/PlantSearchActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sigi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlantSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plant_search) // 적절한 레이아웃 파일로 수정
+    }
+}
